6d56d0db9332c1d5f63f69f84cd9390f80703563
mr.p. khamperngjai <superpck@yahoo.com>
2015-10-12 13:27:45 +0700

    update

--- include/db.connect.php
+++ include/db.connect.php
@@ -2,9 +2,5 @@
     $dbconnect = new mysqli($db["host"],$db["username"], $db["password"], $db["dbname"],$db["port"])
     	or die ('Could not connect to the database server' . mysqli_connect_error());
 
-    if ($dbconnect->connect_errno) {
-        echo "Failed to connect to MySQL: (" . $dbconnect->connect_errno . ") " . $dbconnect->connect_error;
-        exit;
-    }
     $dbconnect->query("SET NAMES ".$db["charset"]);
 ?>
