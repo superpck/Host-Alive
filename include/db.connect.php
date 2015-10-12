d0be88fe83418d904f96b79588415467882a6608
mr.p. khamperngjai <superpck@yahoo.com>
2015-10-12 13:26:02 +0700

    update

--- include/db.connect.php
+++ include/db.connect.php
@@ -1,10 +1,7 @@
 <?php
-    include ($CurrentRoot.'/include/db.php');
-
     $dbconnect = new mysqli($db["host"],$db["username"], $db["password"], $db["dbname"],$db["port"])
     	or die ('Could not connect to the database server' . mysqli_connect_error());
 
-//    $dbconnect = new mysqli($db["host"],$db["username"], $db["password"], $db["dbname"],$db["port"]);
     if ($dbconnect->connect_errno) {
         echo "Failed to connect to MySQL: (" . $dbconnect->connect_errno . ") " . $dbconnect->connect_error;
         exit;
